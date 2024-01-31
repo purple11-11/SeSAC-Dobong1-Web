09591e2105939477dd93ce4175ded86b114e1a49
young <purple11_11@naver.com>
2024-01-31 11:27:15 +0900

    update: 회원가입, 로그인 axios GET, POST 실습 해설

--- 08_dynamic/1.29/app.js
+++ 08_dynamic/1.29/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = 8000;
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
@@ -66,13 +66,44 @@ app.post("/login", (req, res) => {
   const id = "yyi9546";
   const pw = "1234";
   console.log(req.body);
-  let success = false;
+  let isSuccess = false;
 
   if (req.body.id === id && req.body.pw === pw) {
     success = true;
   }
 
-  res.send(success);
+  res.send(isSuccess);
+});
+
+// 실습문제 리더님 코드
+const id = "pororo";
+const pw = "1234";
+app.get("/practice1", (req, res) => {
+  res.render("leaderCode/practice1");
+});
+
+app.get("/practice2", (req, res) => {
+  res.render("leaderCode/practice2");
+});
+
+app.get("/axios-practice1", (req, res) => {
+  console.log(req.query);
+  res.send(req.query);
+});
+
+app.post("/axios-practice2", (req, res) => {
+  console.log(req.body);
+  // 서버의 계정정보와, 클라이언트의 계정정보가 일치하는지
+  const { id: clientId, password: clientPw } = req.body;
+  if (clientId === id && clientPw === pw) {
+    res.send({
+      userInfo: req.body,
+      // ...req.body,
+      isSuccess: true,
+    });
+  } else {
+    res.send({ isSuccess: false });
+  }
 });
 
 app.listen(PORT, () => {
