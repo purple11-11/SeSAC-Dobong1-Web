3ebd1fcd4989f78d0620edef5a7ffa1128fe86a2
young <purple11_11@naver.com>
2024-02-02 10:40:47 +0900

    update: 수업 해설 내용 추가

--- 09_multerPractice/app.js
+++ 09_multerPractice/app.js
@@ -13,10 +13,12 @@ app.use("/static", express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// 폴더 생성 후 업로드, BUT 파일명이 알수 없는 이름으로 올라감
 const upload = multer({
   dest: "uploads/",
 });
 
+// dest 없이 destination으로 업로드 경로를 지정하면 폴더 생성은 안됨
 const uploadDetail = multer({
   storage: multer.diskStorage({
     destination: (req, file, done) => {
@@ -25,6 +27,9 @@ const uploadDetail = multer({
     filename: (req, file, done) => {
       const extension = path.extname(file.originalname);
       done(null, path.basename(file.originalname, extension) + Date.now() + extension);
+      // path.basename(path [, ext]) : 파일의 이름만 표시하고 싶다면 basename의 두 번째 인자로 파일의 확장자(ext)를 넣어준다.
+      // => return: 'path'
+      // path.basename(path) => return 'path.ext'
     },
   }),
   limits: { fileSize: 5 * 1024 * 1024 },
