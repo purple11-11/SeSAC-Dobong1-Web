02290d967891e54cf4c8243cce5652f946a345f3
young <purple11_11@naver.com>
2024-02-08 08:23:07 +0900

    fix: result 문장 수정

--- practice_mvc/controller/Cuser.js
+++ practice_mvc/controller/Cuser.js
@@ -6,7 +6,7 @@ exports.main = (req, res) => {
 
 exports.login = (req, res) => {
   console.log("model: ", User.userInfo()); // model/User의 정보
-  console.log("input: ", req.body); // 클라이언트 정보
+  console.log("client: ", req.body); // 클라이언트 정보
   const { id, pw } = req.body;
   if (id === User.userInfo()[0].id && pw === User.userInfo()[0].pw) {
     res.send({
