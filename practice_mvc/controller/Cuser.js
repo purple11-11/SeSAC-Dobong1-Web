adc2906517df92a83034b880493c267519feb2f6
young <purple11_11@naver.com>
2024-02-08 14:20:03 +0900

    add: 실습2 - DB(문자열) 불러오는 방식의 로그인

--- practice_mvc/controller/Cuser.js
+++ practice_mvc/controller/Cuser.js
@@ -4,18 +4,37 @@ exports.main = (req, res) => {
   res.render("index");
 };
 
+/* 실습 1 */
+// exports.login = (req, res) => {
+//   console.log("model: ", User.userInfo()); // model/User의 정보
+//   console.log("client: ", req.body); // 클라이언트 정보
+//   const { id, pw } = req.body;
+//   if (id === User.userInfo()[0].id && pw === User.userInfo()[0].pw) {
+//     res.send({
+//       userInfo: req.body,
+//       isLogin: true,
+//     });
+//   } else {
+//     res.send({
+//       isLogin: false,
+//     });
+//   }
+// };
+
+/* 실습 2 */
 exports.login = (req, res) => {
-  console.log("model: ", User.userInfo()); // model/User의 정보
-  console.log("client: ", req.body); // 클라이언트 정보
   const { id, pw } = req.body;
-  if (id === User.userInfo()[0].id && pw === User.userInfo()[0].pw) {
-    res.send({
-      userInfo: req.body,
-      isLogin: true,
-    });
-  } else {
-    res.send({
-      isLogin: false,
-    });
+  const user = User.user.split("\n");
+  console.log(user);
+  for (let i = 0; i < user.length; i++) {
+    if (user[i].indexOf(id + "//") !== -1 && user[i].indexOf("//" + pw + "//") !== -1) {
+      res.send({
+        id: user[i].split("//")[2],
+        isLogin: true,
+      });
+    }
   }
+  res.send({
+    isLogin: false,
+  });
 };
